764ec7d2ac484ff568c44a4729bd62c18d9b93d1
Mariusz Barczak <Mariusz.Barczak@solidigmtechnology.com>
2023-04-15 22:06:56 +0200

    [RFC] lib/ftl: Add placeholder for non-vss nv cache device
    Change-Id: I522e0ef2ff924cd10dd930caa205f851517fdb4d
    Signed-off-by: Mariusz Barczak <Mariusz.Barczak@solidigmtechnology.com>

---
+++ lib/ftl/nvc/ftl_nvc_bdev_non_vss.c
@@ -0,0 +1,43 @@
+/*   SPDX-License-Identifier: BSD-3-Clause
+ *   Copyright 2023 Solidigm All Rights Reserved
+ */
+
+#include "ftl_nvc_dev.h"
+#include "ftl_core.h"
+#include "ftl_layout.h"
+
+static bool
+is_bdev_compatible(struct spdk_ftl_dev *dev, struct spdk_bdev *bdev)
+{
+	if (spdk_bdev_get_md_size(bdev) != 0) {
+		/* Bdev's metadata is invalid size */
+		return false;
+	}
+
+	if (ftl_md_xfer_blocks(dev) * spdk_bdev_get_md_size(bdev) > FTL_ZERO_BUFFER_SIZE) {
+		FTL_ERRLOG(dev, "Zero buffer too small for bdev %s metadata transfer\n",
+			   spdk_bdev_get_name(bdev));
+		return false;
+	}
+
+	return true;
+}
+
+static void
+tune_layout_region(struct spdk_ftl_dev *dev, struct ftl_layout_region *region)
+{
+	/* This device doesn't support VSS, disable it */
+	region->vss_blksz = 0;
+}
+
+struct ftl_nv_cache_device_desc nvc_bdev_non_vss = {
+	.name = "bdev-non-vss",
+	.features = {
+		.vss = false,
+	},
+	.ops = {
+		.is_bdev_compatible = is_bdev_compatible,
+		.tune_layout_region = tune_layout_region,
+	}
+};
+FTL_NV_CACHE_DEVICE_TYPE_REGISTER(nvc_bdev_non_vss)
